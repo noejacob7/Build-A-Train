ad88685d743708fb56b5072f9c46d4a8f231e334
Ryan <89624595+RyanPadamadan@users.noreply.github.com>
2022-05-21 23:23:42 +1000

    Changing the collectible
    Changing the collectible to a person, which represents the people getting picked up by the train and a cart gets added

--- game.py
+++ game.py
@@ -6,27 +6,15 @@ Created on Wed Apr 25 15:19:25 2018
 """
 import pygame, random
 import numpy as np
-import os, sys
-
-
-def base_path(path):
-    if getattr(sys, 'frozen', None):
-        basedir = sys._MEIPASS
-    else:
-        basedir =  os.path.abspath(os.path.dirname(__file__))
-    return os.path.join(basedir, path)
-
 
 class Settings:
     def __init__(self):
-        
-        # Change the width and height to make a bigger screen
+        #changed first 2 value, to make the screen bigger
         self.width = 80
         self.height = 50
         #changing the rect length to accomodate the new sprite image
         self.rect_len = 40
-        
-# Created a new class to account for an external background image
+
 class Background(pygame.sprite.Sprite):
     #new class to add a background image
     def __init__(self, image_file, location):
@@ -36,13 +24,13 @@ class Background(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
 
-class Snake:
+class Train:
     def __init__(self):
         #loading the required images for the sprites head
-        self.image_left = pygame.image.load(base_path('images/trainHeadLeft.png'))
-        self.image_right = pygame.image.load(base_path('images/trainHeadRight.png'))
+        self.image_left = pygame.image.load('images/trainHeadLeft.png')
+        self.image_right = pygame.image.load('images/trainHeadRight.png')
         #loading the required image for the sprites tail
-        self.tail = pygame.image.load(base_path('images/trainTail.png'))
+        self.tail = pygame.image.load('images/trainTail.png')
 
         #variable used to cycle through images for the train body
         self.num = 0
@@ -51,15 +39,18 @@ class Snake:
         self.initialize()
 
     def initialize(self):
+        #initial position of the train
         self.position = [6, 6]
+        #to fix the bug that cause the game to end if the train crashes on the left
+        self.facing = "right"
+        #the positions of each part of the train, the length of this list is the length of the train
         self.segments = [[6 - i, 6] for i in range(3)]
         self.score = 0
-        self.facing = "right"
 
     def blit_body(self, x, y, screen):
         #loading the different coloured images of the train
         train_image = 'images/trainBody' + str(self.num % 8) + '.png'
-        self.image_body = pygame.image.load(base_path(train_image))
+        self.image_body = pygame.image.load(train_image)
         #incrementing the number to cycle through the images
         self.num += 1
         #displaying the required image and the coordinates of the train
@@ -80,7 +71,7 @@ class Snake:
             screen.blit(self.image_right, (x, y))
 
         #self.carry_on remembers the current image of the sprites head
-        #so as the train continues moving downwards or upwards, the correct image is still displayed
+        #so as the train continues moving downwards, the correct image is still displayed
 
         #direction is downward, and the sprite was previously moving right
         #the orientation of the head of the sprite remains right
@@ -112,9 +103,9 @@ class Snake:
         
         #sprite already turned and continues to move upward
         elif self.facing == "up":
-            screen.blit(self.carry_on, (x, y)) 
+            screen.blit(self.carry_on, (x, y))
             
-     def blit_tail(self, x, y, screen):
+    def blit_tail(self, x, y, screen):
         #subtracting the last and second last segments x and y coordinates, and based
         #on that the direction of the tail of train changes
         tail_direction = [self.segments[-2][i] - self.segments[-1][i] for i in range(2)]
@@ -125,15 +116,15 @@ class Snake:
         elif tail_direction == [-1, 0]:
             screen.blit(self.tail, (x, y))  
         else:
-            screen.blit(self.tail, (x, y)) 
+            screen.blit(self.tail, (x, y))  
     
     def blit(self, rect_len, screen):
-        self.blit_head(self.segments[0][0]*rect_len, self.segments[0][1]*rect_len, screen)                
+        self.blit_head(self.segments[0][0]*(rect_len), self.segments[0][1]*(rect_len), screen)
+
         for position in self.segments[1:-1]:
-            self.blit_body(position[0]*rect_len, position[1]*rect_len, screen)
-        self.blit_tail(self.segments[-1][0]*rect_len, self.segments[-1][1]*rect_len, screen)                
-            
-    
+            self.blit_body(position[0]*(rect_len), position[1]*(rect_len), screen)
+        self.blit_tail(self.segments[-1][0]*(rect_len), self.segments[-1][1]*(rect_len), screen)                
+             
     def update(self):
         if self.facing == 'right':
             self.position[0] += 1
@@ -145,30 +136,32 @@ class Snake:
             self.position[1] += 1
         self.segments.insert(0, list(self.position))
         
-class Strawberry():
+class Person():
     def __init__(self, settings):
         self.settings = settings
         
-        self.style = str(random.randint(1, 8))
-        self.image = pygame.image.load(base_path('images/food' + str(self.style) + '.bmp'))
+        self.image = pygame.image.load('images/human.png')      
         self.initialize()
         
-    def random_pos(self, snake):
-        self.style = str(random.randint(1, 8))
-        self.image = pygame.image.load(base_path('images/food' + str(self.style) + '.bmp'))
-        
-        self.position[0] = random.randint(0, self.settings.width-1)
-        self.position[1] = random.randint(0, self.settings.height-1)
+    def random_pos(self, train):
+        self.image = pygame.image.load('images/human.png')          
+
+        #the range of the random.randint has been changed to start at 1
+        #this makes sure the person does not form on the edge
+
+        ##new change the random int values are now in this range, based on tested values, 
+        ## due to the change in rect_len
+        self.position[0] = random.randint(1, 28)
+        self.position[1] = random.randint(1, 17)
     
-#         It is causing the food/ collectable to spawn in a small area.
-#         self.position[0] = random.randint(9, 19)
-#         self.position[1] = random.randint(9, 19)
+       #following block of code was causing a bug, spawning the food items only in a small section of the 
+       #map
         
-        if self.position in snake.segments:
-            self.random_pos(snake)
+        if self.position in train.segments:
+            self.random_pos(train)
 
     def blit(self, screen):
-        screen.blit(self.image, [p * self.settings.rect_len for p in self.position])
+        screen.blit(self.image, [p * (self.settings.rect_len) for p in self.position])
    
     def initialize(self):
         self.position = [15, 10]
@@ -179,29 +172,29 @@ class Game:
     """
     def __init__(self):
         self.settings = Settings()
-        self.train = Snake()
-        self.strawberry = Strawberry(self.settings)
+        self.train = Train()
+        self.person = Person(self.settings)
         self.move_dict = {0 : 'up',
                           1 : 'down',
                           2 : 'left',
                           3 : 'right'}       
         
     def restart_game(self):
-        self.snake.initialize()
-        self.strawberry.initialize()
+        self.train.initialize()
+        self.person.initialize()
 
     def current_state(self):         
         state = np.zeros((self.settings.width+2, self.settings.height+2, 2))
         expand = [[0, 1], [0, -1], [-1, 0], [1, 0], [0, 2], [0, -2], [-2, 0], [2, 0]]
         
-        for position in self.snake.segments:
+        for position in self.train.segments:
             state[position[1], position[0], 0] = 1
         
         state[:, :, 1] = -0.5        
 
-        state[self.strawberry.position[1], self.strawberry.position[0], 1] = 0.5
+        state[self.person.position[1], self.person.position[0], 1] = 0.5
         for d in expand:
-            state[self.strawberry.position[1]+d[0], self.strawberry.position[0]+d[1], 1] = 0.5
+            state[self.person.position[1]+d[0], self.person.position[0]+d[1], 1] = 0.5
         return state
     
     def direction_to_int(self, direction):
@@ -213,23 +206,24 @@ class Game:
         
         change_direction = move_dict[move]
         
-        if change_direction == 'right' and not self.snake.facing == 'left':
-            self.snake.facing = change_direction
-        if change_direction == 'left' and not self.snake.facing == 'right':
-            self.snake.facing = change_direction
-        if change_direction == 'up' and not self.snake.facing == 'down':
-            self.snake.facing = change_direction
-        if change_direction == 'down' and not self.snake.facing == 'up':
-            self.snake.facing = change_direction
+        #change if to elif, less number of checks and make the movement a bit more smooth
+        if change_direction == 'right' and not self.train.facing == 'left':
+            self.train.facing = change_direction
+        elif change_direction == 'left' and not self.train.facing == 'right':
+            self.train.facing = change_direction
+        elif change_direction == 'up' and not self.train.facing == 'down':
+            self.train.facing = change_direction
+        elif change_direction == 'down' and not self.train.facing == 'up':
+            self.train.facing = change_direction
 
-        self.snake.update()
-        
-        if self.snake.position == self.strawberry.position:
-            self.strawberry.random_pos(self.snake)
+        self.train.update()
+    
+        if self.train.position == self.person.position:
+            self.person.random_pos(self.train)
             reward = 1
-            self.snake.score += 1
+            self.train.score += 1
         else:
-            self.snake.segments.pop()
+            self.train.segments.pop()
             reward = 0
                 
         if self.game_end():
@@ -240,20 +234,27 @@ class Game:
     def game_end(self):
         end = False
         #hardcoding the values, based on the tested value outputs  
-        
         if self.train.position[0] >= 30 or self.train.position[0] < 0:
             end = True
+            #making sure the train re initializes facing right, fixing bug where it crashes 
+            #on spawn
 
         if self.train.position[1] >= 19 or self.train.position[1] < 0:
+            #making sure the train re initializes facing right, fixing bug where it crashes 
+            #on spawn
             end = True
 
         if self.train.segments[0] in self.train.segments[1:]:
+            #making sure the train re initializes facing right, fixing bug where it crashes 
+            #on spawn
             end = True
+
+
 
         return end
     
     def blit_score(self, color, screen):
         font = pygame.font.SysFont(None, 25)
-        text = font.render('Score: ' + str(self.snake.score), True, color)
+        text = font.render('Score: ' + str(self.train.score), True, color)
         screen.blit(text, (0, 0))
 
